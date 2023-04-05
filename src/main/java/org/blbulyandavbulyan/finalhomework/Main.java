90fa3b5a4e2058bbf5bcdbb6705ce20a7a12cc52
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-04-05 20:24:08 +0400

    Main file added, now you can run my programm

--- src/main/java/org/blbulyandavbulyan/finalhomework/Main.java
+++ src/main/java/org/blbulyandavbulyan/finalhomework/Main.java
@@ -1,2 +1,11 @@
-package org.blbulyandavbulyan.finalhomework;public class Main {
+package org.blbulyandavbulyan.finalhomework;
+
+import static org.blbulyandavbulyan.finalhomework.StringArrayTransformer.transformArrayByLength;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] array = InputOutputStringArray.readStringArray();
+        InputOutputStringArray.printStringArray(transformArrayByLength(array, 3));
+    }
+
 }
